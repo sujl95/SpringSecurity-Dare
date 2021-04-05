1126984990b102ae4b8828ef0655d7ead7aeefdc
sujl95 <sujl95@naver.com>
2021-04-05 22:07:34 +0900

    [#2] SpringSecurity 데어프로그래밍 시큐리티 설정

---
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+				.antMatchers("/user/**").authenticated()
+				.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll() // 다른 요청들 모든 사용자가 접근 가능
+				.and()
+				.formLogin()
+				.loginPage("/login"); // login 페이지로 이동
+
+	}
+}
--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,5 +11,41 @@ public class IndexController {
 	public String index() {
 
 		return "index";
+	}
+
+	@GetMapping("/user")
+	@ResponseBody
+	public String user() {
+		return "user";
+	}
+
+	@GetMapping("/admin")
+	@ResponseBody
+	public String admin() {
+		return "admin";
+	}
+
+	@GetMapping("/manager")
+	@ResponseBody
+	public String manager() {
+		return "manager";
+	}
+
+	@GetMapping("/login")
+	@ResponseBody
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/join")
+	@ResponseBody
+	public String join() {
+		return "join";
+	}
+
+	@GetMapping("/join-proc")
+	@ResponseBody
+	public String joinProc() {
+		return "회원가입 완료됨!!";
 	}
 }
